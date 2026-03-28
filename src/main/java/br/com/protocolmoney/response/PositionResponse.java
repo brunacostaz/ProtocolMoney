3aa2ccbaf81436bc39d6f076f098187b260dc967
brunacostaz <brunacstcandeias@gmail.com>
2026-03-28 11:19:34 -0300

    feat(response): cria a classe de resposta da posição para o protocolo SOAP

--- src/main/java/br/com/protocolmoney/response/PositionResponse.java
+++ src/main/java/br/com/protocolmoney/response/PositionResponse.java
@@ -1,4 +1,49 @@
 package br.com.protocolmoney.response;
 
+import br.com.protocolmoney.model.InvestorPosition;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "positionResponse")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class PositionResponse {
+
+    private String message;
+    private boolean success;
+    private InvestorPosition data;
+
+    public PositionResponse() {}
+
+    public PositionResponse(String message, boolean success, InvestorPosition data) {
+        this.message = message;
+        this.success = success;
+        this.data = data;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public InvestorPosition getData() {
+        return data;
+    }
+
+    public void setData(InvestorPosition data) {
+        this.data = data;
+    }
 }
