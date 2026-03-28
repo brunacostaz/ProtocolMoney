07a620857a7e204a35bb8cc9706eed3c20ae030e
brunacostaz <brunacstcandeias@gmail.com>
2026-03-28 13:00:05 -0300

    feat(publisher): publica o serviço da API SOAP de investimentos

--- src/main/java/br/com/protocolmoney/publisher/InvestmentPublisher.java
+++ src/main/java/br/com/protocolmoney/publisher/InvestmentPublisher.java
@@ -1,4 +1,18 @@
 package br.com.protocolmoney.publisher;
 
+import br.com.protocolmoney.service.InvestmentService;
+
+import javax.xml.ws.Endpoint;
+
 public class InvestmentPublisher {
+
+    public static void main(String[] args) {
+
+        String url = "http://localhost:8080/investments";
+        System.out.println("Subindo o servidor ProtocolMoney!");
+        Endpoint.publish(url, new InvestmentService());
+
+        System.out.println("Serviço publicado com sucesso e rodando em:");
+        System.out.println(url + "?wsdl");
+    }
 }
