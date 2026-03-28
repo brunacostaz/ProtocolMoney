22c4718dfb89db398f5d2539477a96ce6a5f8c86
brunacostaz <brunacstcandeias@gmail.com>
2026-03-28 12:02:41 -0300

    feat(service): implementa o método para resgatar os investimentos

--- src/main/java/br/com/protocolmoney/service/InvestmentService.java
+++ src/main/java/br/com/protocolmoney/service/InvestmentService.java
@@ -8,6 +8,7 @@ import br.com.protocolmoney.response.PositionResponse;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.swing.*;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
@@ -76,11 +77,78 @@ public class InvestmentService {
         }
     }
 
+//-------------------------------------------------------------
+
+    @WebMethod
+    public String redemption (
+            @WebParam(name = "personId") String personId,
+            @WebParam(name = "assetCode") String assetCode,
+            @WebParam(name = "amount") double amount
+    ) {
+
+        // Busca o investidor e retorna erro se não encontrar
+        InvestorPosition investor = database.get(personId);
+
+        if (investor == null) {
+            return "ERRO 404: Investidor não encontrado!";
+        }
+
+        // Busca o ativo na carteira do investidor e retorna erro se não existir
+        AssetPosition asset = investor.getInvestorAssets().stream()
+                .filter( a -> a.getAssetCode().equals(assetCode))
+                .findFirst()
+                .orElse(null);
+
+        if ( asset == null ) {
+            return "ERRO 400: Você não possui saldo no produto " + assetCode;
+        }
+
+        // Valida o código do ativo e a data de vencimento do ativo para liberar ou não o resgate
+        try {
+
+            AssetType type = AssetType.valueOf(assetCode);
+            LocalDate dueDate = LocalDate.parse(asset.getDueDate());
+
+            if (!type.canRedemption(dueDate)) {
+                return "Resgate Bloqueado! O vencimento desse produto é apenas em: " + asset.getDueDate();
+            }
+
+        } catch (IllegalArgumentException e) {
+            String assetsAvailable = "";
+            for (AssetType a : AssetType.values()) {
+                assetsAvailable = assetsAvailable + a.name() + "(" + a.getDescription() + ") | ";
+            }
+            return "ERRO: Código do ativo inválido! Ativos disponíveis: " + assetsAvailable;
+        }
+
+        // Valida o saldo disponível para resgatar
+        if ( asset.getBalance() < amount ) {
+            return "Erro 400: Saldo insuficiente para resgatar " + asset.getAssetDescription() + "Saldo disponível: R$ " + asset.getBalance();
+        }
+
+        // Resgata o valor e atualiza a posição consolidada
+        asset.setBalance( asset.getBalance() - amount );
+
+        // Se a posição for zero, remove o ativo da carteira do investidor
+        if ( asset.getBalance() == 0 ) {
+            investor.getInvestorAssets().remove(asset);
+        }
+
+        // Atualiza a posição consolidada, salva no banco e retorna a resposta ao cliente
+        investor.updateBalance();
+        database.put(personId, investor);
+
+        return "SUCESSO: Resgate de R$ " + amount + " no ativo " + asset.getAssetDescription() + " realizado.";
+    }
+
+
+//-------------------------------------------------------------
+
     @WebMethod
     public PositionResponse getPosition(@WebParam(name = "personId") String personId ) {
 
+        // Busca o investidor e retorna um erro ou sucesso com as posições que ele possui
         InvestorPosition investor = database.get(personId);
-
         if (investor == null) {
             return new PositionResponse("Erro 404: Investidor não encontrado!", false, null);
         }
