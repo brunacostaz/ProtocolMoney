66960cbefbbd071f1a585cfbad76c5f111ed478f
brunacostaz <brunacstcandeias@gmail.com>
2026-03-28 12:05:03 -0300

    feat(model): implementa a classe do ativo que o cliente possui
    -> Armazenamos nela o código do ativo, descrição, posição do cliente e
    data de vencimento (ou seja, quando estará disponível para resgate)
    OBS: era pra ter commitado isso antes, mas fui fazendo commit por commit
    e esqueci desse :\#

--- src/main/java/br/com/protocolmoney/model/AssetPosition.java
+++ src/main/java/br/com/protocolmoney/model/AssetPosition.java
@@ -1,4 +1,55 @@
 package br.com.protocolmoney.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
 public class AssetPosition {
+
+    private String assetCode;
+    private String assetDescription;
+    private double balance;
+    private String dueDate;
+
+    public AssetPosition() {
+    }
+
+    public AssetPosition(String assetCode, String assetDescription, double balance, String dueDate) {
+        this.assetCode = assetCode;
+        this.assetDescription = assetDescription;
+        this.balance = balance;
+        this.dueDate = dueDate;
+    }
+
+    public String getAssetCode() {
+        return assetCode;
+    }
+
+    public void setAssetCode(String assetCode) {
+        this.assetCode = assetCode;
+    }
+
+    public String getAssetDescription() {
+        return assetDescription;
+    }
+
+    public void setAssetDescription(String assetDescription) {
+        this.assetDescription = assetDescription;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
 }
